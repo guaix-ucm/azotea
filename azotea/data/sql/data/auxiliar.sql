a27960dbe1fb0d1a01b8fdb51edfef5a1af06694
Rafael Gonzalez <astrorafael@yahoo.es>
2020-04-07 21:19:27 +0200

    generic SQL structure handling

---
+++ azotea/data/sql/data/auxiliar.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+-- Miscelaneous data to be inserted at database creation
+--------------------------------------------------------
+
+INSERT OR REPLACE INTO state_t(state, label) VALUES ( 0, "REGISTERED");
+INSERT OR REPLACE INTO state_t(state, label) VALUES ( 1, "NO DARK SUBSTRACTED");
+INSERT OR REPLACE INTO state_t(state, label) VALUES ( 2, "DARK SUBSTRACTED");
+
