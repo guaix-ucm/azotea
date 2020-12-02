e5a59ff5a13b284265bfb9352de80b3c795144e7
Rafael Gonzalez <astrorafael@yahoo.es>
2020-12-02 11:45:53 +0100

    temp

--- azotenodo/zipea.py
+++ azotenodo/zipea.py
@@ -32,19 +32,17 @@ log = logging.getLogger("azotenodo")
 def upload_to_zenodo(context):
     log.info("===== CUCUUUU ")
     print(context)
-    log.info("===== TRASSSS ")
+    
+    headers = {"Content-Type": "application/json"}
+    params  = {'access_token': context.access_token}
 
-	# headers = {"Content-Type": "application/json"}
- #    params = {'access_token': apikey}
-
-#     r = requests.post('https://sandbox.zenodo.org/api/deposit/depositions',
-#                    params=params,
-#                    json={},
-#                    # Headers are not necessary here since "requests" automatically
-#                    # adds "Content-Type: application/json", because we're using
-#                    # the "json=" keyword argument
-#                    # headers=headers,
-#                    headers=headers)
-# >>> r.status_code
-# 201
-# >>> r.json()
+    # Headers are not necessary here since "requests" automatically
+    # adds "Content-Type: application/json", because we're using
+    # the "json=" keyword argument
+    # headers=headers,
+    r = requests.post(context.url_prefix + 'deposit/depositions',
+            params=params,
+            json={},
+            headers=headers)
+    log.info("===== STATUS CODE = {0}".format(r.status_code))
+    print(r.json())
